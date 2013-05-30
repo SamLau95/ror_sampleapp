3ca96d755fa97c14cc2ab370cc3a545dd03dbcc7
magurt14 <samlau95@gmail.com>
2013-05-30 15:53:26 -0700

    Cucumber set up

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,10 @@ class User < ActiveRecord::Base
 
 
   before_save { email.downcase! }
+  before_save :create_remember_token
+
+  private
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
