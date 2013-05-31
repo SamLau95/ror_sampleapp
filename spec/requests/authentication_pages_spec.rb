88cd9a7ab061522d2500f5b2a3987c80d8d86e8c
magurt14 <samlau95@gmail.com>
2013-05-30 17:36:39 -0700

    Test and signup form refactor

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,8 +7,8 @@ describe "Authentication" do
   describe "signin page" do
     before { visit signin_path }
 
-    it { should have_selector('h1',    text: 'Sign') }
-    it { should have_selector('title', text: 'Sign') }
+    it { should have_h1('Sign In') }
+    it { should have_title('Sign In') }
   end
 
   describe 'sign in' do
@@ -17,7 +17,7 @@ describe "Authentication" do
     describe 'with invalid information' do
       before { click_button 'Sign In' }
 
-      it { should have_selector('title', text: 'Sign') }
+      it { should have_title('Sign') }
       it { should have_error_message('Invalid') }
 
       describe 'after visiting another page' do
@@ -31,7 +31,7 @@ describe "Authentication" do
       let(:user) { FactoryGirl.create(:user) }
       before { valid_signin(user) }
 
-      it { should have_selector('title', text: user.name) }
+      it { should have_title(user.name) }
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign Out', href: signout_path) }
       it { should_not have_link('Sign In', href: signin_path) }
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -11,3 +11,28 @@ RSpec::Matchers.define :have_error_message do |message|
     page.should have_selector('div.alert.alert-error', text: message)
   end
 end
+
+RSpec::Matchers.define :have_success_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-success', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_title do |t|
+  match do |page|
+    page.should have_selector('title', text: t)
+  end
+end
+
+RSpec::Matchers.define :have_h1 do |t|
+  match do |page|
+    page.should have_selector('h1', text: t)
+  end
+end
+
+def fill_in_valid_user_info
+  fill_in "Name",         with: "Example User"
+  fill_in "Email",        with: "user@example.com"
+  fill_in "Password",     with: "foobar"
+  fill_in "Confirmation", with: "foobar"
+end
