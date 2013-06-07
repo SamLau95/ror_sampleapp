eb296d3f0cb3231a9dc8fcec756f36a77de28feb
magurt14 <samlau95@gmail.com>
2013-06-07 13:37:54 -0700

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,7 @@
 #  email      :string(255)
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#
+#  remember_token :string(255)
 
 require 'spec_helper'
 
@@ -25,8 +25,10 @@ describe User do
   it { should respond_to :password_confirmation }
   it { should respond_to :remember_token }
   it { should respond_to :authenticate }
+  it { should respond_to :admin }
 
   it { should be_valid }
+  it { should_not be_admin }
 
   describe 'when name is not present' do
     before { @user.name = '' }
@@ -121,6 +123,14 @@ describe User do
   describe 'remember_token' do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
+  end
+
+  describe 'with admin attribute set to true' do
+    before do
+      @user.save!
+      @user.toggle! :admin
+    end
+    it { should be_admin }
   end
 
 end
